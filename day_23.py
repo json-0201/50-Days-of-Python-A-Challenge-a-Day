778e906d5873666d55fd8122fcab21ff950f48a3
json-0201 <ghks940201@ucla.edu>
2022-12-18 14:10:33 -0800

    Create day_23.py

---
+++ day_23.py
@@ -0,0 +1,57 @@
+# Simple Calculator
+
+class Calculator:
+    """A simple calculator."""
+    def calculator(self):
+        """user inputs two numbers"""
+
+        operation = input("type an operation: add/subtract/multiply/divivde: ")
+        
+        if operation not in ["add", "subtract", "multiply", "divide"]:
+            raise NameError ("please choose a valid operation!")
+        
+        self.num1 = float(input("Enter first number: "))
+        self.num2 = float(input("Enter second number: "))
+
+        if operation == "add":
+            self.add()
+        elif operation == "subtract":
+            self.subtract()
+        elif operation == "multiply":
+            self.multiply()
+        elif operation == "divide":
+            if self.num2 == 0:
+                raise ZeroDivisionError ("cannot divide by zero!")
+            self.divide()
+
+    
+    def add(self):
+        print(f"{self.num1} + {self.num2} = {self.num1+self.num2}")
+    def subtract(self):
+        print(f"{self.num1} - {self.num2} = {self.num1-self.num2}")
+    def multiply(self):
+        print(f"{self.num1} * {self.num2} = {self.num1*self.num2:.2f}")
+    def divide(self):
+        print(f"{self.num1} / {self.num2} = {self.num1/self.num2:.2f}")
+
+calc = Calculator()
+calc.calculator()
+
+
+# Extra Challenge: Multiply Words
+s1 = "love live and laugh"
+s2 = "Hate war love Peace"
+
+def multiply_words(s: str) -> int:
+    """returns the multiplication of length of lowercase words."""
+
+    s_lower = [word for word in s.split() if not any(char.isupper() for char in word)]
+    
+    res = 1
+    for word in s_lower:
+        res *= len(word)
+    
+    return res
+
+print(multiply_words(s1))
+print(multiply_words(s2))
